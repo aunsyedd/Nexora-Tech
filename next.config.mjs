28eeffac184058bcec4c505d6d2859ada4e610b8
Syed Aun <auns5757@gmail.com>
2026-05-29 16:37:49 +0500

    mail and number fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,3 +4,18 @@ const nextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ["**/supabase/**"],
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
